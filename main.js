830495598f9c18613094b560b29e6a8b66261e2b
jorgev259 <jorgev259@outlook.es>
2019-10-01 10:04:53 -0500

    v2

--- main.js
+++ main.js
@@ -1,52 +1,16 @@
+#!/usr/bin/env node
 const opcua = require('node-opcua')
 const program = require('commander')
 const path = require('path')
 const defaults = require('./defaults.json')
 
-/* program
+program
   .option('-u, --user <username>', 'Username', defaults.user)
   .option('-p, --password <password>', 'Password', defaults.password)
   .option('-h, --host <endpoint url>', 'Endpoint url to connect to', defaults.host)
   .option('-f, --file <filename>', 'Specify filename to save the results to', defaults.file)
   .option('-l, --path <path>', 'Specify path to save the results to', defaults.path)
-  .parse(process.argv) */
-
-program
-  .version('0.1.0')
-  .option('-C, --chdir <path>', 'change the working directory')
-  .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
-  .option('-T, --no-tests', 'ignore test hook')
-
-program
-  .command('setup [env]')
-  .description('run setup commands for all envs')
-  .option('-s, --setup_mode [mode]', 'Which setup mode to use')
-  .action(function (env, options) {
-    const mode = options.setup_mode || 'normal'
-    env = env || 'all'
-    console.log('setup for %s env(s) with %s mode', env, mode)
-  })
-
-program
-  .command('exec <cmd>')
-  .alias('ex')
-  .description('execute the given remote cmd')
-  .option('-e, --exec_mode <mode>', 'Which exec mode to use')
-  .action(function (cmd, options) {
-    console.log('exec "%s" using %s mode', cmd, options.exec_mode)
-  }).on('--help', function () {
-    console.log('')
-    console.log('Examples:')
-    console.log('')
-    console.log('  $ deploy exec sequential')
-    console.log('  $ deploy exec async')
-  })
-
-program
-  .command('*')
-  .action(function (env) {
-    console.log('deploying "%s"', env)
-  })
+  .parse(process.argv) 
 
 program.parse(process.argv)
 
